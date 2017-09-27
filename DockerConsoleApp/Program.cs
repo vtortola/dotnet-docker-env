b1024bd58980e93822386e1b25cf468c37c1fa92
Valeriano <vtortola@gmail.com>
2017-09-27 11:23:20 +0200

    Demo project

---
+++ DockerConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DockerConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"TEST={Environment.GetEnvironmentVariable("TEST")}");
+        }
+    }
+}
